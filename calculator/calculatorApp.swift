b571a564d2ae58dc643ce7adf2702ebd9dbe817e
williskim <wbk0009@auburn.edu>
2023-01-29 20:26:30 -0800

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Willis Kim on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
